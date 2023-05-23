bf5e34717f30b01d3cd6dbf1c8c218b74b146cc4
João Pegoraro <hahmraro@tuta.io>
2023-05-23 16:42:24 -0300

    Finalizei integração com Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'firebase_options.dart';
+import 'package:meu_mengao/ui/app/app.dart';
 
 void main() {
   runApp(const MeuMengaoApp());
-}
-
-class MeuMengaoApp extends StatelessWidget {
-  const MeuMengaoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meu Mengão',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: FutureBuilder(
-        future: Future.wait([
-          Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
-          FirebaseAuth.instance.signInAnonymously()
-        ]),
-        builder: (context, snapshot) {
-          if (snapshot.error != null) {
-            return Placeholder(
-              child: Text("ERRO: ${snapshot.error}"),
-            );
-          }
-
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const Placeholder();
-          }
-
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-    );
-  }
 }
---
+++ lib/ui/app/app.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:meu_mengao/firebase_options.dart';
+
+class MeuMengaoApp extends StatelessWidget {
+  const MeuMengaoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu Mengão',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: FutureBuilder(
+        future: Future(() async {
+          await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+          await FirebaseAuth.instance.signInAnonymously();
+        }),
+        builder: (context, snapshot) {
+          if (snapshot.error != null) {
+            return Placeholder(
+              child: Text("ERRO: ${snapshot.error}"),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const Placeholder();
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
